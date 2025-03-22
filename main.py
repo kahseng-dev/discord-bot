be7b1bb10cc3d41ee896a489bce8d2899d5ce997
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-22 15:43:58 +0800

    feat: add basic bot test functions

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+command_prefix = "!"
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.bot(command_prefix, intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"TEST: {bot.user} is online!")
+
+@bot.tree.command(name="say_working", description="TEST COMMAND: literal says its working")
+async def say_working(interaction: discord.Interaction):
+    username = interaction.user.mention
+    await interaction.response.send_message(f"Its working!, {username}")
+
+bot.run(DISCORD_TOKEN)
