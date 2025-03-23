3381efec818988787b89b6bae6b3ab703260cd62
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-23 13:50:46 +0800

    test: base start function

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import discord
 import webserver
 import asyncio
 import logging
+import undetected_chromedriver as uc
 from dotenv import load_dotenv
 from discord import app_commands
 from discord.ext.commands import Bot
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 load_dotenv()
 log = logging.getLogger(__name__)
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+HOST_USERNAME = os.getenv("HOST_USERNAME")
+HOST_PASSWORD = os.getenv("HOST_PASSWORD")
+SERVER_ID = os.getenv("SERVER_ID")
 
 command_prefix = "!"
 intents = discord.Intents.default()
@@ -32,6 +38,37 @@ async def ping(interaction):
 
 @bot.tree.command(name = "start", description = "Starts minecraft server when it is offline")
 async def start(interaction):
+    options = webdriver.ChromeOptions() 
+    options.headless = True
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    driver = uc.Chrome(options=options)
+
+    website = "https://panel.play.hosting/server"
+    website_auth = "https://panel.play.hosting/auth/login"
+
+    website_username = driver.find_element(By.NAME, "username")
+    website_password = driver.find_element(By.NAME, "password")
+    website_loginButton = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/form/div/div/div[4]/button")
+
+    driver.get(website_auth)
+    website_username.send_keys(HOST_USERNAME)
+    website_password.send_keys(HOST_PASSWORD)
+    website_loginButton.click()
+
+    driver.execute_script(f"window.open({website}/{SERVER_ID},'_blank');")
+    driver.switch_to.window(driver.window_handles[1])
+
+    server_status = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[4]/div/div/h2").text
+
+    print(server_status)
+
+    if (server_status == "Server in Limbo"):
+        return
+
+    driver.close()
     await interaction.response.send_message(f"[INFO]: Not implemented yet")
 
 @bot.tree.command(name = "status", description = "Display minecraft server status")
