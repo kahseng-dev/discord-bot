173d4dc059c0a2cb7d7d8d4227f2e0e0f3e5c511
kahseng-dev <kahseng.chew@protonmail.com>
2025-03-24 01:37:30 +0800

    fix: add install commands

--- render-build.sh
+++ render-build.sh
@@ -18,3 +18,6 @@ fi
 
 # be sure to add Chromes location to the PATH as part of your Start Command
 # export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+
+pip install -r requirements.txt
+seleniumbase install chromedriver
